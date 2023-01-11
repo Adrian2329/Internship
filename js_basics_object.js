e4f282089274dc829160874d37ce6bcdbb3e1fa4
Adrian2329 <kopacinsky@bk.ru>
2023-01-11 23:53:53 +0200

    until task 7

---
+++ js_basics_object.js
@@ -0,0 +1,95 @@
+/* //task1
+let user = {};
+console.log(user);
+user.name = "John",
+    console.log(user);
+user.surname = "Smith";
+console.log(user);
+user.name = "Pete";
+console.log(user);
+delete user.name;
+console.log(user); */
+
+/* //task2
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+let salariu = (Object.values(salaries));
+const sum = salariu.reduce((a, b) => {
+    return a + b;
+}, 0);
+console.log(sum); */
+
+/* //task3
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+const multiplyNumeric = () => {
+    for (const key in menu) {
+        if (typeof(menu[key] == 'number')) {
+            menu[key] *= 2;
+        }
+    }
+    console.log(menu);
+}
+multiplyNumeric(menu); */
+
+/* //task4
+const user1 = {
+    name: `User 1`,
+    age: 123
+}
+const user2 = Object.assign({}, user1)
+user2.name = `User 2`;
+user2.age = 321;
+console.log(user1);
+console.log(user2); */
+
+/* //task5
+const info = {
+    name: `Person`,
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: `112`,
+        email: `email @domain.com`,
+    },
+    address: `Moldova`,
+}
+info.contacts.phone = +37312345678;
+delete info.address;
+console.log(info); */
+
+/* //task6
+const arr = [{ x: 1, y: 2 },
+    { x: 3, y: 4 },
+    { x: null, y: 4 },
+    { x: 3, y: undefined },
+]
+const wrong = (obj) => {
+    arr.forEach((item, index) => {
+        for (const key in item) {
+            if (!(item[key])) {
+                arr.splice(index)
+            }
+        }
+    })
+    console.log(arr);
+}
+console.log(wrong()); */
+
+/* //task7
+const person = {
+    name: `Person`,
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: `112`,
+        email: `email@domain.com`
+    },
+    address: `Moldova`
+} */
