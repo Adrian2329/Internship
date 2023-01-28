e0c976c3646a60191a96f07c3a4ba06a0577ab6c
Adrian2329 <kopacinsky@bk.ru>
2023-01-28 17:20:10 +0200

    until doom

---
+++ js_basic.js
@@ -0,0 +1,26 @@
+/* //task1
+const names= [
+  `james`,
+  `jake`];
+
+const json =JSON.stringify(names);
+console.log(json); */
+
+/* //task2
+const power = { 
+  voltage: 250,
+  current: 12 }
+const Json = JSON.stringify(power);
+console.log(power); */
+
+/* //task3
+const json='["james", "jake"]';
+ const arr= JSON.parse(json);
+  console.log(arr); */
+
+ /* //task4
+ const pers = '{"name": "Ion Creanga", "age": 123}'
+  const obj= JSON.parse(pers);
+   console.log(obj); */
+
+  
